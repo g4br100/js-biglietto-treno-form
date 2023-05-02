faaac7283ab6e53947808457e6ff4121a2d26378
user1 <user@email.com>
2023-05-02 12:55:06 +0200

    contenitore biglietto

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 let km, eta, prezzo;
-km = document.getElementById("km").value;
-eta = document.getElementById("eta").value;
-prezzo = km * 0.21;
-console.log(prezzo);
-if (eta < 18) {
-    prezzo = prezzo - ( prezzo * 20 ) / 100;
-} else if (eta >= 65) {
-    prezzo = prezzo - ( prezzo * 40 ) / 100;
+gen.addEventListener ("click"), function() {
+    km = document.getElementById("km");
+    eta = document.getElementById("eta");
+    prezzo = km * 0.21;
+    console.log(prezzo);
+    if (eta < 18) {
+        prezzo = prezzo - ( prezzo * 20 ) / 100;
+    } else if (eta >= 65) {
+        prezzo = prezzo - ( prezzo * 40 ) / 100;
+    }
+    document.getElementById('prezzo').innerHTML = "Il prezzo è " + prezzo.toFixed(2) + "€";
+    console.log(
+        `${prezzo}`
+    )
 }
-document.getElementById('prezzo').innerHTML = "Il prezzo è " + prezzo.toFixed(2) + "€";
-console.log(
-    `${prezzo}`
-)
